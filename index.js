a61891de37043ccec5a7f0621450c332ce3e85ee
Min Cho <minsangcho19@gmail.com>
2021-12-15 16:18:49 -0800

    commit tree 325cc5528237d4cf5b9b0c2e56d5e0ffa796d512

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arr) => {return (arr.slice(0,2))}
+const returnLastTwoDrivers = (arr) => {return (arr.slice(-2))}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(factor){
+    function multiplier(fare) {
+        return factor * fare;
+    }
+    return multiplier
+}
+
+const fareDoubler = (fare) => createFareMultiplier(2);
+const fareTripler = (fare) => createFareMultiplier(3);
+
+
+// selectDifferentDrivers()
+
+function selectDifferentDrivers(drivers, func1){
+    if(func1 === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(drivers);
+    }
+    else return returnLastTwoDrivers(drivers);
+}
